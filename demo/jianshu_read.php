3caf3d399ecccf1ddc653f1910781290451ee0b9
林德标 <lindb@weipaitang.com>
2018-05-25 17:36:35 +0800

    Fix

--- demo/jianshu_read.php
+++ demo/jianshu_read.php
@@ -122,9 +122,9 @@ function toSee(int $i,array $result,int $num,string $ip = ""){
         //requests::set_proxy($ip);
 
         requests::set_referer($key['referer']);//必须将referer调成当前页面
+        requests::$error = '';
         rePost:
         requests::post($ulas,json_encode($data));
-
         if(!empty(requests::$error)){
             echo "Post Error ".date('Y-m-d H:i:s').":".PHP_EOL;
             echo requests::$error.PHP_EOL;
